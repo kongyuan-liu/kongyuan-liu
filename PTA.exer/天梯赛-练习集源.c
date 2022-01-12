64f7f7b1a52c03a5e847f6be459270623aef5de0
kongyuan-liu <w0509151799@outlook.com>
2022-01-12 15:04:55 +0800

    4th day

--- PTA.exer/天梯赛-练习集源.c
+++ PTA.exer/天梯赛-练习集源.c
@@ -101,7 +101,8 @@ int main()
 	//}
 	//printf("%s", a);
 }*/
-#include<stdio.h>
+
+/*#include<stdio.h>
 #include<math.h>
 int main()
 {
@@ -110,4 +111,97 @@ int main()
 	double num = 0;
 	num = pow(2, n);
 	printf("%g", floor(num));
+}*/
+//L1-016 查验身份证
+/*#include<stdio.h>
+int main()
+{
+	int n, sum = 0, i, j, flag = 0;
+	scanf("%d", &n);
+	int num1[20] = { 7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2 };
+	char num3[20] = { 0 };
+	char num4[] = { '1','0','X','9','8','7','6','5','4','3','2' };
+	for (i = 0; i < n; i++)
+	{
+		sum = 0;
+		scanf("%s", num3);
+		for (j = 0; j < 17; j++)
+		{
+			sum += (num3[j] - '0') * num1[j];
+		}
+		sum %= 11;
+		if (num4[sum] != num3[17])
+		{
+			flag = 1;
+			printf("%s\n",num3);
+		}
+	}
+	if (flag == 0)
+		printf("All passed");
+}*/
+//L1-017 到底有多二
+/*#include<stdio.h>
+#include<string.h>
+int main()
+{
+	double n;
+	int i, num = 0;
+	int count = 0, len;
+	char a[51] = { 0 };
+	double cnt = 1;
+	scanf("%s", a);
+	len = strlen(a);
+	num = len;
+	if (a[0] == '-') {
+		cnt += 0.5;
+		num--;
+	}
+	if ((a[len - 1] - '0') % 2 == 0)
+		cnt *= 2;
+	for (i = 0; i < len; i++)
+	{
+		if (a[i] == '2')
+			count++;
+	}
+	n = count / (num * 1.0) * cnt;
+	printf("%.2lf", n * 100);
+	printf("%%");
+}*/
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 9;
+//	double b = 0.5,c;
+//	c = a * b;
+//	printf("%lf", c);
+//}
+#include<stdio.h>
+int main()
+{
+	int a, b;
+	int q, w, e, r;
+	scanf("%d %d", &a, &b);
+	int cnt1 = 0, cnt2 = 0;
+	int n, i, j;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d %d %d %d", &q, &w, &e, &r);
+		if (w == q + e) cnt1++;
+		if (r == q + e) cnt2++;
+		if (cnt1 > a || cnt2 > b) {
+			if (cnt1 > a)
+			{
+				printf("A\n");
+				printf("%d", cnt2);
+				return 0;
+			}
+			if (cnt2 > b)
+			{
+				printf("B\n");
+				printf("%d", cnt1);
+				return 0;
+			}
+		}
+	}
 }
