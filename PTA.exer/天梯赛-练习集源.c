b03c3c57ce6ffd26c368ad0a3c826599aafaf1bb
kongyuan-liu <w0509151799@outlook.com>
2022-01-26 20:57:23 +0800

    26th

--- PTA.exer/天梯赛-练习集源.c
+++ PTA.exer/天梯赛-练习集源.c
@@ -721,7 +721,7 @@ int main()
 	return 0;
 }*/
 //L1-059 敲笨钟
-#include<stdio.h>
+/*#include<stdio.h>
 #include<string.h>
 int Judge(char* a, int i);
 void Printf(char* a, int i);
@@ -776,11 +776,11 @@ void Printf(char* a, int i)
 			count++;
 		}
 		if (count == 3) {
-/*			for (k = 0; k < strlen(ter); k++) {
-				a[++j] = ter[k];
-			}
-			printf("%s\n", a);
-			return 0;*/ //若後三個詞的長度大於ter數組的長度，則無法完全覆蓋
+			//for (k = 0; k < strlen(ter); k++) {
+			//	a[++j] = ter[k];
+			//}
+			//printf("%s\n", a);
+			//return 0; //若後三個詞的長度大於ter數組的長度，則無法完全覆蓋
 			k = j;
 			break;
 		}
@@ -789,6 +789,36 @@ void Printf(char* a, int i)
 		printf("%c", a[j]);
 	}
 	printf("%s\n", ter);
+}*/
+//L1-063 吃鱼还是吃肉
+#include<stdio.h>
+int main()
+{
+	int n, s, t, a;//用a表示男女，s表示身高，t表示体重，n表示循环次数
+	scanf("%d", &n);
+	while (n--)
+	{
+		scanf("%d %d %d", &a, &s, &t);
+		if (a == 1)//判断是不是男孩
+		{
+			if (s < 130) printf("duo chi yu! ");
+			if (s == 130) printf("wan mei! ");
+			if (s > 130) printf("ni li hai! ");
+			if (t < 27) printf("duo chi rou!\n");
+			if (t == 27) printf("wan mei!\n");
+			if (t > 27) printf("shao chi rou!\n");
+		}
+		if (a == 0)//判断是不是女孩
+		{
+			if (s < 129) printf("duo chi yu! ");
+			if (s == 129) printf("wan mei! ");
+			if (s > 129) printf("ni li hai! ");
+			if (t < 25) printf("duo chi rou!\n");
+			if (t == 25) printf("wan mei!\n");
+			if (t > 25) printf("shao chi rou!\n");
+		}
+	}
+	return 0;
 }
 
 
